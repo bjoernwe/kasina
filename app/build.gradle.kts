4d11a78bbf53437fc0728382df5fcc3fbeb97699
Björn <bjoernwe@users.noreply.github.com>
2024-08-28 20:10:58 +0200

    :arrow_up: Upgrade dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -68,8 +69,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics)
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
     kapt(libs.hilt.android.compiler)
 
