198267c9f50f11e467f301ee84e07f004cc1d9cc
Björn <bjoernwe@users.noreply.github.com>
2024-09-14 18:39:20 +0200

    :children_crossing: Keep screen on

--- app/src/main/java/dev/upaya/kasina/MainActivity.kt
+++ app/src/main/java/dev/upaya/kasina/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.upaya.kasina
 
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -50,5 +51,15 @@ class MainActivity : ComponentActivity() {
         }
 
         return super.onKeyUp(keyCode, event)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
