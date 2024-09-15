661e1ca90849f745eb37927b7f9a8424e124974e
Björn <bjoernwe@users.noreply.github.com>
2024-09-15 15:12:27 +0200

    🧑‍💻 Enable Timber logging

--- app/src/main/java/dev/upaya/kasina/FlashApplication.kt
+++ app/src/main/java/dev/upaya/kasina/FlashApplication.kt
@@ -2,8 +2,16 @@ package dev.upaya.kasina
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 
 @HiltAndroidApp
 class FlashApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
