0090800d41de9d4cd020d7ffb75348aadcfba959
Björn <bjoernwe@users.noreply.github.com>
2024-09-26 20:58:30 +0200

    🚸 Fix manually changed flashlight state

--- app/src/main/java/dev/upaya/kasina/flashlight/FlashLightStateController.kt
+++ app/src/main/java/dev/upaya/kasina/flashlight/FlashLightStateController.kt
@@ -5,6 +5,7 @@ import dev.upaya.kasina.inputkeys.PressableKeyState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -25,22 +26,18 @@ class FlashLightStateController @Inject constructor(
     private var flashLightOnOffJob: Job? = null
 
     fun start(scope: CoroutineScope) {
-        flashLightStateJob = launchFlashLightStateJob(scope)
         flashLightOnOffJob = launchFlashLightOnOffJob(scope)
+        flashLightStateJob = launchFlashLightStateJob(scope)
     }
 
     fun turnOff() {
         _flashLightState.update { FlashLightState.OFF }
     }
 
-    private fun launchFlashLightStateJob(scope: CoroutineScope) = scope.launch {
-        inputKeyHandler.volumeKeysState.collect { keyEvent ->
-            _flashLightState.update { currentState ->
-                transitionToNextState(currentState, keyEvent)
-            }
-        }
-    }
-
+    /**
+     * This job subscribes to our own flashlight state and turns the actual flashlight on/off
+     * accordingly.
+     */
     private fun launchFlashLightOnOffJob(scope: CoroutineScope) = scope.launch {
         _flashLightState.collect { state ->
             when (state) {
@@ -49,10 +46,57 @@ class FlashLightStateController @Inject constructor(
             }
         }
     }
+
+    /**
+     * This jobs keeps the flashlight state updated according to the incoming events.
+     */
+    private fun launchFlashLightStateJob(scope: CoroutineScope) = scope.launch {
+        inputKeyHandler.volumeKeysState
+            .combine(flashLight.isOn) { keyEvent, isFlashLightOn -> keyEvent to isFlashLightOn }
+            .collect { keyAndFlash ->
+                val (keyEvent, isFlashLightOn) = keyAndFlash
+                updateFlashLightState(keyEvent, isFlashLightOn)
+            }
+    }
+
+    private fun updateFlashLightState(keyEvent: PressableKeyState, isFlashLightOn: Boolean) {
+        _flashLightState.update { currentState ->
+            calcFlashlightState(currentState, keyEvent, isFlashLightOn)
+        }
+    }
+
 }
 
 
-private fun transitionToNextState(currentState: FlashLightState, keyEvent: PressableKeyState): FlashLightState {
+private fun calcFlashlightState(currentState: FlashLightState, keyEvent: PressableKeyState, isFlashLightOn: Boolean): FlashLightState {
+
+    // Did the user turn the flashlight on/off manually?
+    if (!isStateCongruentWithFlashLight(currentState, isFlashLightOn))
+        return fixedFlashlightState(isFlashLightOn)
+
+    return transitionToNextFlashlightState(currentState, keyEvent)
+}
+
+
+/**
+ * Our state can get out of sync with the actual flashlight when the user manually turns the
+ * flashlight on or off outside of the app. If that happens, we update our state accordingly.
+ */
+private fun isStateCongruentWithFlashLight(currentState: FlashLightState, isFlashLightOn: Boolean): Boolean {
+    return when (currentState to isFlashLightOn) {
+        FlashLightState.OFF         to true  -> false  // Flash is on but we don't know it
+        FlashLightState.ON_SWITCHED to false -> false  // Flash is off but we don't know it
+        else -> true
+    }
+}
+
+
+private fun fixedFlashlightState(isFlashLightOn: Boolean): FlashLightState {
+    return if (isFlashLightOn) FlashLightState.ON_SWITCHED else FlashLightState.OFF
+}
+
+
+private fun transitionToNextFlashlightState(currentState: FlashLightState, keyEvent: PressableKeyState): FlashLightState {
     return when (currentState to keyEvent) {
         FlashLightState.OFF               to PressableKeyState.PRESSED      -> FlashLightState.TRANSITION_TO_ON
         FlashLightState.TRANSITION_TO_ON  to PressableKeyState.PRESSED_LONG -> FlashLightState.ON_HOLDING
