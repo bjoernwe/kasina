19ab112906c864d73ca70597de98179cdf493fc1
Björn <bjoernwe@users.noreply.github.com>
2024-10-06 17:31:50 +0200

    ♻️ Extract functions

---
+++ app/src/main/java/dev/upaya/kasina/ui/FlameAndTimer.kt
@@ -0,0 +1,68 @@
+package dev.upaya.kasina.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.upaya.kasina.data.Session
+import dev.upaya.kasina.data.SessionState
+import dev.upaya.kasina.data.SessionState.ACTIVE_OFF
+import kotlinx.coroutines.delay
+import java.util.Locale
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+
+@Composable
+internal fun FlameAndTimer(
+    sessionState: SessionState,
+    currentSession: Session?,
+    modifier: Modifier = Modifier,
+) {
+    var sessionDuration by remember { mutableStateOf(0.toDuration(DurationUnit.SECONDS)) }
+
+    LaunchedEffect(currentSession) {
+        while (true) {
+            sessionDuration = calcSessionDuration(currentSession)
+            delay(100L)
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .wrapContentHeight()
+    ) {
+        FlameIcon(
+            sessionState = sessionState,
+            modifier = Modifier
+                .fillMaxWidth(.45f)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(
+            modifier = Modifier.padding(8.dp),
+        )
+        Text(
+            text = if (sessionState == ACTIVE_OFF) String.format(
+                Locale.ROOT,"%02d:%02d:%02d",
+                sessionDuration.inWholeHours,
+                sessionDuration.inWholeMinutes % 60,
+                sessionDuration.inWholeSeconds % 60,
+            ) else "",
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
--- app/src/main/java/dev/upaya/kasina/ui/MainScreen.kt
+++ app/src/main/java/dev/upaya/kasina/ui/MainScreen.kt
@@ -4,43 +4,28 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import dev.upaya.kasina.R
-import dev.upaya.kasina.data.SessionState.ACTIVE_OFF
 import dev.upaya.kasina.data.SessionState.INACTIVE
 import dev.upaya.kasina.flashlight.FlashlightViewModel
 import dev.upaya.kasina.ui.theme.FlashKasinaTheme
-import kotlinx.coroutines.delay
-import java.util.Locale
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,16 +37,7 @@ fun MainScreen() {
     val sessionState by flashlightViewModel.sessionState.collectAsState(INACTIVE)
     val currentSession by flashlightViewModel.currentSession.collectAsState(initial = null)
 
-    var sessionDuration by remember { mutableStateOf(0.toDuration(DurationUnit.SECONDS)) }
-
     val sessionActiveAlpha: Float by animateFloatAsState(if (sessionState == INACTIVE) 1f else 0f, label = "alpha")
-
-    LaunchedEffect(currentSession) {
-        while (true) {
-            sessionDuration = calcSessionDuration(currentSession)
-            delay(100L)
-        }
-    }
 
     Scaffold(
         topBar = {
@@ -87,24 +63,7 @@ fun MainScreen() {
                     .fillMaxWidth()
                     .graphicsLayer(alpha = sessionActiveAlpha)
             ) {
-                Row(
-                    modifier = Modifier
-                        .wrapContentHeight()
-                        .padding(12.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.baseline_info_24),
-                        contentDescription = "Info",
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                    )
-                    Spacer(
-                        modifier = Modifier.padding(4.dp),
-                    )
-                    Text(
-                        text = "Press/hold physical buttons to start",
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    )
-                }
+                InfoText()
             }
 
             Box(
@@ -113,33 +72,10 @@ fun MainScreen() {
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                Column(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                ) {
-                    FlameIcon(
-                        sessionState = sessionState,
-                        modifier = Modifier
-                            .fillMaxWidth(.45f)
-                            .align(Alignment.CenterHorizontally)
-                    )
-                    Spacer(
-                        modifier = Modifier.padding(8.dp),
-                    )
-                    Text(
-                        text = if (sessionState == ACTIVE_OFF) String.format(
-                            Locale.ROOT,"%02d:%02d:%02d",
-                            sessionDuration.inWholeHours,
-                            sessionDuration.inWholeMinutes % 60,
-                            sessionDuration.inWholeSeconds % 60,
-                        ) else "",
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                    )
-                }
+                FlameAndTimer(
+                    sessionState = sessionState,
+                    currentSession = currentSession,
+                )
             }
 
             Surface(
