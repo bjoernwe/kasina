68f146274accbb8efd0d0774bc578eeefd3db89f
Björn <bjoernwe@users.noreply.github.com>
2024-10-11 20:40:10 +0200

    🔊 Logging (#25)

--- app/src/main/java/dev/upaya/kasina/data/SessionStateRepository.kt
+++ app/src/main/java/dev/upaya/kasina/data/SessionStateRepository.kt
@@ -2,6 +2,10 @@ package dev.upaya.kasina.data
 
 import android.content.Context
 import androidx.room.Room
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.upaya.kasina.flashlight.FlashlightStateController
 import kotlinx.coroutines.CoroutineDispatcher
@@ -38,10 +42,13 @@ class SessionStateRepository @Inject constructor(
     private val _currentSession = MutableStateFlow<Session?>(null)
     val currentSession: StateFlow<Session?> = _currentSession
 
+    private val firebaseAnalytics: FirebaseAnalytics = Firebase.analytics
+
     suspend fun startStoringSessions(dispatcher: CoroutineDispatcher = Dispatchers.IO) {
         withContext(dispatcher) {
             launchJobForCurrentSessionState()
             launchJobForStoringSessions()
+            launchJobForFirebaseAnalytics()
         }
     }
 
@@ -58,6 +65,17 @@ class SessionStateRepository @Inject constructor(
     private fun CoroutineScope.launchJobForStoringSessions() = launch {
         _currentSession.collect {
             storeSession(it)
+        }
+    }
+
+    private fun CoroutineScope.launchJobForFirebaseAnalytics() = launch {
+        _currentSession.collect { session ->
+            if (session == null || session.isIncomplete)
+                return@collect
+            firebaseAnalytics.logEvent("session") {
+                param("duration_on", session.onDuration.toDouble())
+                param("duration_off", session.offDuration.toDouble())
+            }
         }
     }
 
